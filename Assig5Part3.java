3f0b46bc66662397faf0d5f8ef94c1b1dd5f0bed
NoChute <jengblom@csumb.edu>
2017-05-29 18:00:53 -0400

    Updates to part 3

--- Assig5Part3.java
+++ Assig5Part3.java
@@ -1,156 +1,174 @@
 package assignment5;
+
 import java.util.Random;
 
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
-import javax.swing.border.*;
 
 public class Assig5Part3 {
    
    static final int NUM_PLAYERS = 2;
    static final int NUM_CARDS_PER_HAND = 5;
-   static JLabel[] computerLabels = new JLabel[NUM_CARDS_PER_HAND];
-   static JButton[] humanLabels = new JButton[NUM_CARDS_PER_HAND];  
-   static JLabel[] playedCardLabels  = new JLabel[NUM_PLAYERS]; 
-   static JLabel[] playLabelText  = new JLabel[NUM_PLAYERS]; 
-
-   
    static final int COMPUTER = 0;
-   static final int PLAYER = 0;
+   static final int PLAYER = 1;
+   int humWin = 0;
+   int compWin = 0;
    
+   private JLabel[] computerLabels   = new JLabel[NUM_CARDS_PER_HAND];
+   private JButton[] humanLabels     = new JButton[NUM_CARDS_PER_HAND];  
+   private JLabel[] playedCardLabels = new JLabel[NUM_PLAYERS]; 
+   private JLabel[] playLabelText    = new JLabel[NUM_PLAYERS]; 
+   private GUICard  guicard          = new GUICard();
    private int numPacksPerDeck        = 1;
    private int numJokersPerPack       = 0;
    private int numUnusedCardsPerPack  = 0;
    private Card[] unusedCardsPerPack  = null;
-   
    private CardGameFramework highCardGame = new CardGameFramework( 
             numPacksPerDeck, numJokersPerPack,  
             numUnusedCardsPerPack, unusedCardsPerPack, 
-            NUM_PLAYERS, NUM_CARDS_PER_HAND);
-   
+            NUM_PLAYERS, NUM_CARDS_PER_HAND);     
    private CardTable myCardTable = new CardTable("CardTable", 
             NUM_CARDS_PER_HAND, NUM_PLAYERS); 
-   
-   
+            
+   private Hand computerHand;
+   private Hand playerHand;
+
    public static void main(String[] args) {
       
-      Assig5Part3 program = new Assig5Part3();
-      GUICard guicard = new GUICard(); 
+      Assig5Part3 newgame = new Assig5Part3();
+      newgame.myCardTable.setVisible(true);
       
-      //Computer is player 0, Human is player 1
-      program.highCardGame.deal();
-      
-      //establish main frame in which program will run
-      
-      program.myCardTable.setSize(1000, 600);
-      program.myCardTable.setLocationRelativeTo(null);
-      program.myCardTable.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      
-      program.updateGameTable(program.myCardTable, program.highCardGame);
-        
-     
-      playedCardLabels[0] = new JLabel(guicard.getIcon(null));
-      playedCardLabels[1] = new JLabel(guicard.getIcon(null));
-      
-      playLabelText[0] = new JLabel("Computer", JLabel.CENTER);
-      playLabelText[1] = new JLabel("You", JLabel.CENTER);
-      
-      program.myCardTable.pnlPlayArea.add(playLabelText[0]);
-      program.myCardTable.pnlPlayArea.add(playLabelText[1]);
-      
-      program.myCardTable.pnlPlayArea.add(playedCardLabels[0]);
-      program.myCardTable.pnlPlayArea.add(playedCardLabels[1]);
-      
-      //program.myCardTable.pack();
-      program.myCardTable.setVisible(true);
    }
    
-   private Card generateRandomCard() {
-      return new Deck().inspectCard(new Random().nextInt(56));
+   public Assig5Part3() {
+  
+      this.highCardGame.deal();
+      this.computerHand = highCardGame.getHand(COMPUTER);
+      this.playerHand = highCardGame.getHand(PLAYER);
+      this.computerHand.sort();
+      this.playerHand.sort();
+      
+      this.myCardTable.setLocationRelativeTo(null);
+      this.myCardTable.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      
+      this.playedCardLabels[0] = new JLabel(guicard.getIcon(null));
+      this.playedCardLabels[1] = new JLabel(guicard.getIcon(null));
+      this.playLabelText[0] = new JLabel("Computer", JLabel.CENTER);
+      this.playLabelText[1] = new JLabel("You", JLabel.CENTER);
+      
+      updateGameTable();
+      
+      this.myCardTable.pack();
+      
    }
 
-   public void updateGameTable(CardTable gametable, CardGameFramework cardgame, Card cardPlayed) {
-      
-      Hand computerHand = cardgame.getHand(COMPUTER);
-      Hand playerHand = cardgame.getHand(PLAYER);
-      GUICard guicard = new GUICard();
+   private void updateGameTable() {
       
       // Update JLabels for Computer.
-      gametable.pnlComputerHand.removeAll();
-      
+      myCardTable.pnlComputerHand.removeAll();
       for (int x = 0; x < computerHand.getNumCards(); x++) {
-         gametable.pnlComputerHand.add(new JLabel(guicard.getBackCardIcon()));
+         myCardTable.pnlComputerHand.add(new JLabel(guicard.getBackCardIcon()));
       }
       
-      playerHand.sort();
-      //Update JButton for Player
-      gametable.pnlHumanHand.removeAll();
+      //Draw Play Area
+      myCardTable.cmpPlayArea.removeAll();
+      myCardTable.cmpPlayArea.add(playedCardLabels[0]);
+      myCardTable.hmnPlayArea.removeAll();
+      myCardTable.hmnPlayArea.add(playedCardLabels[1]);
+      myCardTable.pnlPlayArea.add(playLabelText[0]);
+      myCardTable.pnlPlayArea.add(playLabelText[1]);
+      
+      
+      //Update JButtons for Player
+      myCardTable.pnlHumanHand.removeAll();
+      
       for (int x = 0; x < playerHand.getNumCards(); x++) {
-         humanLabels[x] = new JButton(guicard.getIcon(playerHand.inspectCard(x)));
+         humanLabels[x] = new JButton(guicard.getIcon(this.playerHand.inspectCard(x)));
          humanLabels[x].setActionCommand(String.valueOf(x));
          humanLabels[x].addActionListener( new CardListener() );
-         gametable.pnlHumanHand.add(humanLabels[x]);
-         
+         myCardTable.pnlHumanHand.add(humanLabels[x]);
       }
       
-      gametable.pnlPlayArea.remove(playedCardLabels[0]);
-      gametable.pnlPlayArea.remove(playedCardLabels[1]);
-      
-      playedCardLabels[0] = new JLabel(guicard.getIcon(cardPlayed));
-      playedCardLabels[1] = new JLabel(guicard.getIcon(cardPlayed));
-      
-      myCardTable.pnlPlayArea.add(playedCardLabels[0]);
-      myCardTable.pnlPlayArea.add(playedCardLabels[1]);
-      
+      myCardTable.pnlComputerHand.validate();
+      myCardTable.pnlComputerHand.repaint();
       myCardTable.pnlPlayArea.validate();
       myCardTable.pnlPlayArea.repaint();
-      
-      gametable.pnlHumanHand.validate();
-      gametable.pnlHumanHand.repaint();
-      
-      
-   }
-   
-public void updateGameTable(CardTable gametable, CardGameFramework cardgame) {
-      
-      Hand computerHand = cardgame.getHand(COMPUTER);
-      Hand playerHand = cardgame.getHand(PLAYER);
-      GUICard guicard = new GUICard();
-      
-      // Update JLabels for Computer.
-      gametable.pnlComputerHand.removeAll();
-      
-      for (int x = 0; x < computerHand.getNumCards(); x++) {
-         gametable.pnlComputerHand.add(new JLabel(guicard.getBackCardIcon()));
-      }
-      
-      playerHand.sort();
-      //Update JButton for Player
-      gametable.pnlHumanHand.removeAll();
-      for (int x = 0; x < playerHand.getNumCards(); x++) {
-         humanLabels[x] = new JButton(guicard.getIcon(playerHand.inspectCard(x)));
-         humanLabels[x].setActionCommand(String.valueOf(x));
-         humanLabels[x].addActionListener( new CardListener() );
-         gametable.pnlHumanHand.add(humanLabels[x]);
-         
-      }
-
-      gametable.pnlHumanHand.validate();
-      gametable.pnlHumanHand.repaint();
-      
+      myCardTable.pnlHumanHand.validate();
+      myCardTable.pnlHumanHand.repaint();
      
    }
    
    private class CardListener implements ActionListener {
       public void actionPerformed(ActionEvent e) {
-         Card cardPlayed = highCardGame.getHand(PLAYER).playCard(Integer.valueOf(e.getActionCommand()));
-         playerPlays(cardPlayed);
-         updateGameTable(myCardTable, highCardGame, cardPlayed);
+         Card cardPlayed = playerHand.playCard(
+               Integer.valueOf(e.getActionCommand()));
+         playerPlays(cardPlayed);         
       }
    }
    
+   
    private void playerPlays(Card cardPlayed) {
+      playedCardLabels[1] = new JLabel(guicard.getIcon(cardPlayed));
+      computerPlays(cardPlayed);      
+   }
+   
+   private void decidePlayWinner(Card playerCard, Card computerCard) {
+      if (playerCard.getValue() == 'A' && computerCard.getValue() == 'X') {
+         
+      }
+      else if (playerCard.compareTo(computerCard) == 1) {
+         humWin += 1;
+      }
+      else if (playerCard.compareTo(computerCard) == 1) {
+         compWin += 1;
+      }
+      else {
+         humWin += 0;
+         compWin += 0;
+      }
+      
+   }
+   
+   private String decideGameWinner(int humWin, int compWin) {
+      String winner = "";
+      if (humWin < compWin) {
+         winner = "You lost. Hang your head in shame.";
+      }
+      else if (humWin == compWin) {
+         winner = "It was a tie.";
+      }
+      else {
+         winner = "You won! Congratulations!";
+      }
+      return winner;
+   }
+   
+   private void computerPlays(Card cardPlayerPlayed) {
+      Card computerCard = computerPicksACard(cardPlayerPlayed);
+      playedCardLabels[0] = new JLabel(guicard.getIcon(computerCard));
+      updateGameTable();
+   }
+   
+   private Card computerPicksACard(Card cardPlayerPlayed) {
+      
+      Card cardToPlay = null;
+      if (cardPlayerPlayed.getValue() == 'A') {
+         // Card was an Ace, play the lowest card, 
+         // Which could be a joker
+            cardToPlay = this.computerHand.playCard(this.computerHand.getNumCards() - 1);
+      } else {
+         for ( int x = this.computerHand.getNumCards()-1; x >=0; x--) {
+            if (this.computerHand.inspectCard(x).compareTo(cardPlayerPlayed) == 1) {
+               cardToPlay = this.computerHand.playCard(x);
+               break;
+            }
+         }
+         if (cardToPlay == null) {
+            cardToPlay = this.computerHand.playCard(this.computerHand.getNumCards() - 1);
+         }
+      }
+      return cardToPlay;
       
    }
 }
